0227c3af69a731a42c0e5974a08055f4e7115f39
HelgaPataki1983 <88514237+HelgaPataki1983@users.noreply.github.com>
2021-10-22 17:39:12 +0300

    first commit in project

---
+++ src/main/java/Calculator/Calculator.java
@@ -0,0 +1,10 @@
+package Calculator;
+
+public class Calculator {
+    public int sum(int a, int b){
+        return a+b;
+    }
+    public  int multiply(int j, int k){
+        return j*k;
+    }
+}
